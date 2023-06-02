40fd7b65e925a6b1a3367e3720e0967e8cb1f092
Alexander-Solonar <Alexander-Solonar_solo991@ukr.net>
2023-06-02 10:58:02 +0300

    fix 404

--- src/pages/Tweets.js
+++ src/pages/Tweets.js
@@ -1,16 +1,16 @@
-import { Link, useSearchParams } from "react-router-dom";
-import Button from "../components/Button/Button";
-import TweetsList from "../components/tweetsList/";
-import * as API from "../services/API";
-import { useEffect, useState } from "react";
-import Filter from "../components/filter";
+import { Link } from 'react-router-dom';
+import Button from '../components/Button/Button';
+import TweetsList from '../components/tweetsList/';
+import * as API from '../services/API';
+import { useEffect, useState } from 'react';
+import Filter from '../components/filter';
 
 const Tweets = () => {
   const [tweets, setTweets] = useState([]);
-
+  const [limit, setLimit] = useState(3);
   const [isButton, setIsButton] = useState(false);
-  const [searchParams, setSearchParams] = useSearchParams();
-  const limit = searchParams.get("limit") ?? "3";
+  // const [searchParams, setSearchParams] = useSearchParams();
+  // const limit = searchParams.get("limit") ?? "3";
 
   useEffect(() => {
     (async () => {
@@ -29,7 +29,8 @@ const Tweets = () => {
   }, [limit]);
 
   const handleChangePage = () => {
-    setSearchParams((prevState) => ({ ...prevState, limit: +limit + 3 }));
+    // setSearchParams((prevState) => ({ ...prevState, limit: +limit + 3 }));
+    setLimit(prevState => prevState + 3);
   };
 
   return (
